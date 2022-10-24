97e9a5b09edc3e896069595ed1c9a9c2420d11ee
leandro <eu@cefet-rj.br>
2022-10-24 09:18:34 -0300

    teste

--- conta.py
+++ conta.py
@@ -13,7 +13,27 @@ class Conta:
         
     def __init__(self,numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
-        self.numero=numero
-        self.titular=titular
-        self.saldo=saldo
-        self.limite=limite
+        self.__numero=numero
+        self.__titular=titular
+        self.__saldo=saldo
+        self.__limite=limite
+        
+        #dois _ na frente do atributo, ele vira private
+        
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo,self.__titular))
+        
+    def deposita(self,valor):
+        self.__saldo += valor
+        
+    def saca(self,valor):
+        self.__saldo -= valor
+        
+ #   def transfere(self,valor,origem,destino):
+ #       origem.saca(valor)
+ #       destino.deposita(valor)
+ 
+    def transfere(self,valor,origem,destino):
+        self.saca(valor)
+        destino.deposita(valor)
+    
