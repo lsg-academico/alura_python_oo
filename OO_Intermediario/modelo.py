12053c308e2ac86040350d22da922262d655de75
leandro <eu@cefet-rj.br>
2022-10-25 11:45:29 -0300

    if ternario

---
+++ OO_Intermediario/modelo.py
@@ -0,0 +1,53 @@
+class Programa:
+    def __init__(self,nome,ano):
+        self._nome=nome.title()
+        self.ano=ano
+        self._likes=0
+
+    @property
+    def likes(self):
+        return self._likes
+    
+    def dar_like(self):
+        self._likes+=1
+            
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self,novo_nome):
+        self._nome =novo_nome.title()
+
+#############################################
+class Filme (Programa):
+    def __init__(self,nome,ano,duracao):
+        super().__init__(nome,ano)  
+        self.duracao=duracao
+     
+            
+#############################################       
+class Serie (Programa):
+    def __init__(self,nome,ano,temporadas):
+        super().__init__(nome,ano)  
+        self.temporadas=temporadas
+    
+#############################################
+
+            
+vingadores=Filme('vingadores - guerra infinita',2018,160)
+vingadores.dar_like()
+#print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} - {vingadores.likes}')
+#print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano}'
+#      f' - Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+
+atlanta=Serie('atlanta',2018,2)
+atlanta.dar_like()
+#print(f'{atlanta.nome} - {atlanta.ano} - {atlanta.temporadas} - {vingadores.likes}')
+
+filmes_e_series=[vingadores,atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa,'duracao') else programa.temporadas
+    print(f'{programa.nome} -{detalhes}  D - {programa.likes}')
